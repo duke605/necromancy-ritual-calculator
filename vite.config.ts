d097cef53f276a56dfdcdf635377d746cd88e476
Cole <duke605@users.noreply.github.com>
2023-08-24 13:52:17 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/necromancy-ritual-calculator',
   resolve: {
     alias: {
       '$data': path.resolve(__dirname, './data'),
